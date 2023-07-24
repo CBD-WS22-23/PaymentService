628ebc49e2cb839d45c5d7db61b1c79491efc125
Steven Diestel <82208488+Izamii@users.noreply.github.com>
2023-07-24 03:48:00 +0200

    Changed request mapping uri to work with the api gateway

--- src/main/java/edu/timebandit/PaymentService/port/user/controller/PaymentController.java
+++ src/main/java/edu/timebandit/PaymentService/port/user/controller/PaymentController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/pys/api/v1")
 public class PaymentController {
 
     @Autowired
